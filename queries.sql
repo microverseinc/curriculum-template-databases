dbccba4d652be9db906c5d0b1f41057dc251342a
muskan2532150 <rg2532150@gmail.com>
2022-10-22 14:44:53 +0530

    Created table,insert values and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals where name like '%mon' ;
+SELECT name from animals where EXTRACT(YEAR FROM date_of_birth) between 2016 AND 2019;
+SELECT name from animals where neutered='true' AND escape_attempts>='3'; 
+SELECT date_of_birth from animals where name='Agumon' OR name='pikachu';
+SELECT name,escape_attempts from animals where weight_kg>='10.5';
+SELECT * from animals where neutered='true';
+SELECT * from animals where NOT name='Gabumon';
+SELECT * from animals where weight_kg between 10.4 AND 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg real,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
