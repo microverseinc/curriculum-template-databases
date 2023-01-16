448572b483ce66a00941f470150876bda653499d
Ben Sales <adebsa2401@gmail.com>
2023-01-16 15:35:48 +0100

    feat: Write some queries request using aggregation syntax

--- queries.sql
+++ queries.sql
@@ -33,3 +33,10 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO savepoint_name;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(*) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
