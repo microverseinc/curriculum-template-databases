3a8196d9aaec350b0118e198fa897b4add1df83f
Ben Sales <adebsa2401@gmail.com>
2023-01-16 10:58:38 +0100

    feat: Create animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+-- SELECT * from animals WHERE name LIKE '%mon';
+-- SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+-- SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
+-- SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+-- SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+-- SELECT * from animals WHERE neutered = true;
+-- SELECT * from animals WHERE name != 'Gabumon';
+-- SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
