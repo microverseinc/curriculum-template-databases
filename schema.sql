f8b51408130d8082974f82d421e55fa195d4d55b
Oscar Espinoza <37676636+Oscar-Espinoza@users.noreply.github.com>
2023-02-27 23:29:57 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,1 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(100),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5,2)
-);
