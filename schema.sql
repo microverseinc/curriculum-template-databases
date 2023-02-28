b909303d516479b0113e3396e85eaa936df094e5
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-02-27 23:26:10 -0300

    first commit

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(5,2)
 );
